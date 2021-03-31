305bbe39adc2d7d0c7d15f772e269a979e8ac292
Mark Vadasz <markdudevad@gmail.com>
2021-03-31 13:08:05 -0400

    Add Dashboard template from Material-ui

--- src/App.js
+++ src/App.js
@@ -1,12 +1,38 @@
-import './App.css';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import Dashboard from "./dashboard";
+import Home from "./home";
 
 function App() {
   return (
-    <div>
+    <Router>
       <div>
-        Body div
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/dashboard">Dashboard</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Switch>
+          <Route path="/dashboard">
+            <Dashboard />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
       </div>
-    </div>
+    </Router>
   );
 }
 
